763ac082dd62c96234a650836d78bd6b8447c731
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-01-09 00:25:34 +0300

    .

--- Generic/src/main/java/Basket.java
+++ Generic/src/main/java/Basket.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Basket<F extends Fruit> {
+public class Basket<F extends Fruit> implements Iterable<F> {
     private ArrayList<F> list = new ArrayList<>();
+    private static int size = 0;
+
 
     public void add(F fruit) {
         list.add(fruit);
+        size++;
     }
 
     public float getWeight() {
@@ -14,12 +18,37 @@ public class Basket<F extends Fruit> {
         }
         return sum;
     }
-    public int compare(F basket2){
+
+    public int compare(Basket<?> basket2) {
         if (getWeight() > basket2.getWeight()) return 1;
         else if (getWeight() < basket2.getWeight()) return -1;
         else return 0;
     }
-    public void transfer(F basket2){
 
+    public static  void transfer(Basket<F> dest, Basket<F> source) {
+        for (Fruit fruit : source){
+            dest.add(fruit);
+        }
+    }
+
+    public void clear() {
+        list = new ArrayList<>();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < list.size();
+            }
+
+            @Override
+            public F next() {
+                return list.get(index++);
+            }
+        };
     }
 }
